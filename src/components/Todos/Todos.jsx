c4fc43a77a000f46c04a485580dfdc98663c0bd3
fakef0rest <fakef0rest69@gmail.com>
2022-06-11 14:28:40 +0500

    refactor

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,6 +1,10 @@
+import { useRef } from "react"
+import './Todos.scss';
+
 export const Todos = ({todos, setTodos}) => {
+    const pRef = useRef(null);
     const handleEditTodo = todo => {
-        let editedTodo = prompt('Ozgartirish', todo.text)
+        let editedTodo = prompt('Change', todo.text)
 
         todo.text = editedTodo
 
@@ -12,14 +16,29 @@ export const Todos = ({todos, setTodos}) => {
 
         setTodos([...deletedTodos]);
     }
+
+    const handleCheckTodo = (evt, todoId) => {
+
+        const filteredTodos = todos.map(todo => {
+            const {isCompleted, ...o} = todo;
+            if (todo.id === todoId) {
+                return {isCompleted: evt.target.checked, ...o};
+            } else {
+                return todo;
+            }
+        })
+        setTodos(filteredTodos);
+
+
+    }
     return (
         <>
         {todos.length > 0 && <ul>
                 {todos.map(todo => (
                     <li key={todo.id}>
 
-                        <input type="checkbox" />
-                        <p>{todo.text}</p>
+                        <input type="checkbox" onChange={evt => handleCheckTodo(evt, todo.id)}/>
+                        <p ref={pRef} className={todo.isCompleted ? 'lineThrough' : null}>{todo.text}</p>
                         <button onClick={() => handleEditTodo(todo)}>Edit</button>    
                         <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>    
 
